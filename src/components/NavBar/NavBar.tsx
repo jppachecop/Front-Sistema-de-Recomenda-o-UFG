7b229712bc02011b68f3ef0767da8399f9dad84a
João Paulo Pacheco <jppacheco09@hotmail.com>
2021-10-22 14:24:21 -0300

    Ajuste no ícone da NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -47,7 +47,7 @@ export const NavBar = () => {
                             <Avatar
                                 size={'sm'}
                                 src={
-                                    'https://avatars.dicebear.com/api/male/username.svg'
+                                    'https://cdn.dribbble.com/users/24078/screenshots/15522433/media/e92e58ec9d338a234945ae3d3ffd5be3.jpg?compress=1&resize=400x300'
                                 }
                             />
                         </Box>
